175747fd0a4b170f0a9665973b93e26ce3507598
vitas26072001 <78877306+vitas26072001@users.noreply.github.com>
2021-12-04 21:27:19 +0300

    Delete about.cpp

--- reports/Kroshchuk Viktor/lab3/src/PROJECT/about/about.cpp
+++
@@ -1,9 +0,0 @@
-#include "about.h"
-#include <QMessageBox>
-
-void about()
-{
-    QMessageBox msgBox;
-    msgBox.setText("Программу сделал студент группы ПО-5 Крощук Виктор\nДанная программа - Игра Lines");
-    msgBox.exec();
-}

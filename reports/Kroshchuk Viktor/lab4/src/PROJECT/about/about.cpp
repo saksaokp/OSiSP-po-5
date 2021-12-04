d7d512f433a95ca37834c456aeb65343c9ee6203
vitas26072001 <78877306+vitas26072001@users.noreply.github.com>
2021-12-04 21:23:04 +0300

    Delete about.cpp

--- reports/Kroshchuk Viktor/lab4/src/PROJECT/about/about.cpp
+++
@@ -1,9 +0,0 @@
-#include "about.h"
-#include <QMessageBox>
-
-void about()
-{
-    QMessageBox msgBox;
-    msgBox.setText("Программу сделала студентка группы ПО-5 Нерода Александра\nПрограмма является MP3 плеером");
-    msgBox.exec();
-}

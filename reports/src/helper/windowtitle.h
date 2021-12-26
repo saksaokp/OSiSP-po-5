335503f8aed376208049ad84e3d0ec8905f2e331
XraiseD <78748611+XraiseD@users.noreply.github.com>
2021-12-26 19:26:41 +0300

    Delete reports directory

--- reports/src/helper/windowtitle.h
+++
@@ -1,9 +0,0 @@
-#ifndef WINDOWTITLE_H
-#define WINDOWTITLE_H
-
-#include "helper_global.h"
-#include <QString>
-
-extern "C" HELPER_EXPORT QString set_window_name();
-
-#endif // WINDOWTITLE_H
--- reports/src/sudoku-about/about.h
+++
@@ -1,9 +0,0 @@
-#ifndef ABOUT_H
-#define ABOUT_H
-
-#include "about_global.h"
-#include <QWidget>
-
-extern "C" void showAboutWindow(QWidget* parent);
-
-#endif // ABOUT_H
